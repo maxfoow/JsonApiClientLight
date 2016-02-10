e5a434c42baf8b8fecc75b5d84cafff1ff8ee42b
Maxime CHARLES <maxfoow@gmail.com>
2016-02-10 15:23:25 +0100

    adding tooltip on interface.

--- src/Orion.JsonApiClientLight/IJsonApiClientLight.cs
+++ src/Orion.JsonApiClientLight/IJsonApiClientLight.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.IO;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
@@ -6,13 +7,97 @@ using System.Threading.Tasks;
 namespace Orion.JsonApiClientLight {
 	public interface IJsonApiClientLight {
 		Dictionary<string, string> Headers { get; }
-		Task<HttpResponseMessage> GetAsync( string url,  object parameters, CancellationToken? token = null);
-		Task<HttpResponseMessage> PostAsync( string url,  object data, CancellationToken? token = null);
-		Task<HttpResponseMessage> PutAsync( string url,  object data, CancellationToken? token = null);
-		Task<HttpResponseMessage> DeleteAsync( string url,  object data, CancellationToken? token = null);
-		Task<T> GetAsync<T>( string url,  object parameters, CancellationToken? token = null);
-		Task<T> PostAsync<T>( string url,  object data, CancellationToken? token = null);
-		Task<T> PutAsync<T>( string url,  object data, CancellationToken? token = null);
-		Task<T> DeleteAsync<T>( string url,  object data, CancellationToken? token = null);
+		RetryPolicy RetryPolicy { get; set; }
+		/// <summary>
+		/// Make HTTP Get request asynchronous
+		/// </summary>
+		/// <param name="url">url of target</param>
+		/// <param name="parameters">query string parameters</param>
+		/// <param name="token">cancellation token</param>
+		/// <returns>HttpResponseMessage of request</returns>
+		Task<HttpResponseMessage> GetAsync(string url, object parameters, CancellationToken? token = null);
+		/// <summary>
+		/// Make HTTP POST request asynchronous
+		/// </summary>
+		/// <param name="url">url of target</param>
+		/// <param name="data">data for the request's content</param>
+		/// <param name="token">cancellation token</param>
+		/// <returns>HttpResponseMessage of request</returns>
+		Task<HttpResponseMessage> PostAsync(string url, object data, CancellationToken? token = null);
+		/// <summary>
+		/// Make HTTP PUT request asynchronous
+		/// </summary>
+		/// <param name="url">url of target</param>
+		/// <param name="data">data for the request's content</param>
+		/// <param name="token">cancellation token</param>
+		/// <returns>HttpResponseMessage of request</returns>
+		Task<HttpResponseMessage> PutAsync(string url, object data, CancellationToken? token = null);
+		/// <summary>
+		/// Make HTTP DELETE request asynchronous
+		/// </summary>
+		/// <param name="url">url of target</param>
+		/// <param name="parameters">query string parameters</param>
+		/// <param name="token">cancellation token</param>
+		/// <returns>HttpResponseMessage of request</returns>
+		Task<HttpResponseMessage> DeleteAsync(string url, object parameters, CancellationToken? token = null);
+
+		/// <summary>
+		/// Make HTTP GET request asynchronous with automatic deserialization of response.
+		/// </summary>
+		/// <typeparam name="T">Deserialization type</typeparam>
+		/// <param name="url">url of target</param>
+		/// <param name="parameters">query string parameters</param>
+		/// <param name="token">cancellation token</param>
+		/// <returns>Instance of T filled with response content</returns>
+		Task<T> GetAsync<T>(string url, object parameters, CancellationToken? token = null);
+		/// <summary>
+		/// Make HTTP POST request asynchronous with automatic deserialization of response.
+		/// </summary>
+		/// <typeparam name="T">Deserialization type</typeparam>
+		/// <param name="url">url of target</param>
+		/// <param name="data">data for the request's content</param>
+		/// <param name="token">cancellation token</param>
+		/// <returns>Instance of T filled with response content</returns>
+		Task<T> PostAsync<T>(string url, object data, CancellationToken? token = null);
+		/// <summary>
+		/// Make HTTP PUT request asynchronous with automatic deserialization of response.
+		/// </summary>
+		/// <typeparam name="T">Deserialization type</typeparam>
+		/// <param name="url">url of target</param>
+		/// <param name="data">data for the request's content</param>
+		/// <param name="token">cancellation token</param>
+		/// <returns>Instance of T filled with response content</returns>
+		Task<T> PutAsync<T>(string url, object data, CancellationToken? token = null);
+		/// <summary>
+		/// Make HTTP DELETE request asynchronous with automatic deserialization of response.
+		/// </summary>
+		/// <typeparam name="T">Deserialization type</typeparam>
+		/// <param name="url">url of target</param>
+		/// <param name="parameters">query string parameters</param>
+		/// <param name="token">cancellation token</param>
+		/// <returns>Instance of T filled with response content</returns>
+		Task<T> DeleteAsync<T>(string url, object parameters, CancellationToken? token = null);
+
+		/// <summary>
+		/// Make HTTP Request, default POST, for upload a file.
+		/// </summary>
+		/// <param name="url">url of target</param>
+		/// <param name="file">file's stream content</param>
+		/// <param name="filename">the name of file, default "file"</param>
+		/// <param name="httpMethod">Request's httpMethod</param>
+		/// <param name="token">cancellation token</param>
+		/// <returns>HttpResponseMessage of target</returns>
+		Task<HttpResponseMessage> UploadFile(string url, Stream file, string filename = null, HttpMethod httpMethod = null, CancellationToken? token = null);
+		/// <summary>
+		/// Make HTTP Request, default POST, for upload a file with automatic deserialization.
+		/// </summary>
+ 		/// <typeparam name="T">Deserialization type</typeparam>
+		/// <param name="url">url of target</param>
+		/// <param name="file">file's stream content</param>
+		/// <param name="filename">the name of file, default "file"</param>
+		/// <param name="httpMethod">Request's httpMethod</param>
+		/// <param name="token">cancellation token</param>
+		/// <returns>Instance of T filled with response content</returns>
+		Task<T> UploadFile<T>(string url, Stream file, string filename = null, HttpMethod httpMethod = null, CancellationToken? token = null);
 	}
 }
